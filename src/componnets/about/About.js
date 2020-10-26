78268330ca62de5e04b473dcf8508226e5019c15
Ben <bigriffin94@outlook.com>
2020-10-26 11:40:34 -0700

    About me page coming along

--- src/componnets/about/About.js
+++ src/componnets/about/About.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import './About.css';
+import cristian from '../../images/cristian.jpg';
 
 function About() {
     return (
         <div className='about-container'>
 
-            <div className='about-content'>
             <h1 className='about-header'>about me</h1>
+            
+            <div className='about-content'>
             <div className='about-left-content'>
-                <p className='about-para'>CRISTIAN GARCIA</p>
-                <p className='about-para-cert'>NASM certified personal trainer</p>
+
+
+                <img 
+                    className='about-img'
+                    src={cristian}
+                    alt="about"
+                />
+     
+
             </div>
 
             <div className="about-middle-content">
+            <p className='about-para'>CRISTIAN GARCIA</p>
+                <p className='about-para-cert'>NASM certified personal trainer</p>
                 <p>
                     I am a freelance NASM certified Personal Trainer.
                     I enjoy helping others achieve their goals through fitness.
