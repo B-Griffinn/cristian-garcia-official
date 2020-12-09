abb353cf0f9ad89b8bcfa42469f257985f0cacc7
Ben <bigriffin94@outlook.com>
2020-12-09 15:50:20 -0800

    Adjusted Footer: social icons spaced properly created by added

--- src/componnets/footer/Footer.js
+++ src/componnets/footer/Footer.js
@@ -1,97 +1,59 @@
-import React from 'react';
-import './Footer.css';
+import React from "react";
+import "./Footer.css";
 // import Button  from '../button/Button';
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <div className='footer-container'>
-      {/* <section className='footer-subscription'>
-        <p className='footer-subscription-heading'>
-          Join the ISR newsletter!
-        </p>
-        <p className='footer-subscription-text'>
-          You can unsubscribe at any time.
-        </p>
-        <div className='input-areas'>
-          <form>
-            <input
-              className='footer-input'
-              name='email'
-              type='email'
-              placeholder='Your Email'
-            />
-            <Button buttonStyle='btn--outline'>Subscribe</Button>
-          </form>
-        </div>
-      </section> */}
-      <div className='footer-links'>
-        <div className='footer-link-wrapper'>
-          {/* <div className='footer-link-items'>
-            <h2>About Us</h2>
-            <Link to='/sign-up'>How it works</Link>
-            <Link to='/'>Testimonials</Link>
-            <Link to='/'>Careers</Link>
-            <Link to='/'>Investors</Link>
-            <Link to='/'>Terms of Service</Link>
-          </div> */}
-          <div className='footer-link-items'>
+    <div className="footer-container">
+      <div className="footer-links">
+        <div className="footer-link-wrapper">
+          <div className="footer-link-items">
             <h2>Contact Us</h2>
-            <Link to='/contact'>Contact</Link>
-            {/* <Link to='/'>Support Us</Link> */}
-            {/* <Link to='/'>Sponsorships</Link> */}
-            {/* <Link to='/'>Schedule</Link> */}
+            <Link to="/contact">Contact</Link>
           </div>
         </div>
-        <div className='footer-link-wrapper'>
-          {/* <div className='footer-link-items'>
-            <h2>Videos</h2>
-            <Link to='/'>Submit Video</Link>
-            <Link to='/'>Ambassadors</Link>
-            <Link to='/'>Agency</Link>
-            <Link to='/'>Influencer</Link>
-          </div> */}
-          <div className='footer-link-items'>
+        <div className="footer-link-wrapper">
+          <div className="footer-link-items">
             <h2>Social Media</h2>
             <a
-                target="blank"
-                href='https://www.instagram.com/cristiannoh15/?hl=en'
+              target="blank"
+              href="https://www.instagram.com/cristiannoh15/?hl=en"
             >
-                Instagram
+              Instagram
             </a>
           </div>
         </div>
       </div>
-      <section className='social-media'>
-        <div className='social-media-wrap'>
-          <div className='footer-logo'>
-            <Link to='/' className='social-logo'>
-              cristian garcia
-              {/* <i className='fas fa-microphone-alt' /> */}
-            </Link>
+      <section className="social-media">
+        <div className="social-media-wrap">
+          <div className="social-icons">
+            <a
+              className="social-icon-link instagram"
+              href="https://www.instagram.com/cristiannoh15/"
+              target="blank"
+              aria-label="Instagram"
+            >
+              <i className="fab fa-instagram" />
+            </a>
+            <a
+              className="social-icon-link youtube"
+              href="mailto:cgsaldanasbd@gmail.com"
+              target="blank"
+              aria-label="Envelope"
+            >
+              <i className="fas fa-envelope" />
+            </a>
           </div>
-          <a className='a-tag' target='blank' href="https://bengriffin-chi.vercel.app/">
-          <small className='website-rights'>bigTechnology © 2020</small>
+        </div>
+        <div className="developer-wrapper">
+          <a
+            className="a-tag-developer"
+            target="blank"
+            href="https://bengriffin-chi.vercel.app/"
+          >
+            <small className="website-rights">Created By Ben Griffin</small>
           </a>
-          <div className='social-icons'>
-            <a
-              className='social-icon-link instagram'
-              href='https://www.instagram.com/cristiannoh15/'
-              target='blank'
-              aria-label='Instagram'
-            >
-              <i className='fab fa-instagram' />
-            </a>
-
-            <a
-              className='social-icon-link youtube'
-              href='mailto:cgsaldanasbd@gmail.com'
-              target='blank'
-              aria-label='Envelope'
-            >
-              <i className='fas fa-envelope' />
-            </a>
-          </div>
         </div>
       </section>
     </div>
