35e00716fae84b7f059d4165552daf7bd5d4f6df
Ben <bigriffin94@outlook.com>
2020-12-10 11:43:56 -0800

    Ready for cristian to accept emails

--- src/App.js
+++ src/App.js
@@ -1,36 +1,34 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import Navabar from "./componnets/NavBar/Navbar";
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './componnets/pages/Home';
-import ContactPage from './componnets/pages/ContactPage';
-import ServicesPage from './componnets/pages/ServicesPage';
-import Testimonial from './componnets/testimonials/Testimonial';
-import AboutPage from './componnets/pages/AboutPage';
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./componnets/pages/Home";
+import ContactPage from "./componnets/pages/ContactPage";
+import ServicesPage from "./componnets/pages/ServicesPage";
+import Testimonial from "./componnets/testimonials/Testimonial";
 
 function App() {
   return (
     <>
-    <Router>
-      <Navabar />
+      <Router>
+        <Navabar />
 
-      <Switch>
-        <Route path="/" exact component={Home}/>
-      </Switch>
-      <Switch>
-        <Route path="/contact"  component={ContactPage}/>
-      </Switch>
-      <Switch>
+        <Switch>
+          <Route path="/" exact component={Home} />
+        </Switch>
+        <Switch>
+          <Route path="/contact" component={ContactPage} />
+        </Switch>
+        {/* <Switch>
         <Route path="/about"  component={AboutPage}/>
-      </Switch>
-      <Switch>
-        <Route path="/services"  component={ServicesPage}/>
-      </Switch>
-      <Switch>
-        <Route path="/testimonials"  component={Testimonial}/>
-      </Switch>
-    </Router>
+      </Switch> */}
+        {/* <Switch>
+          <Route path="/services" component={ServicesPage} />
+        </Switch>
+        <Switch>
+          <Route path="/testimonials" component={Testimonial} />
+        </Switch> */}
+      </Router>
     </>
   );
 }
--- src/componnets/forms/useForm.js
+++ src/componnets/forms/useForm.js
@@ -1,56 +1,85 @@
-import { useState, useEffect } from 'react';
-import emailjs from 'emailjs-com';
+import { useState, useEffect } from "react";
+import emailjs from "emailjs-com";
 
 const useForm = (callback, validate) => {
   const [values, setValues] = useState({
-    name: '',
-    email: '',
-    message: ''
+    name: "",
+    email: "",
+    message: "",
   });
   const [errors, setErrors] = useState({});
   const [isSubmitting, setIsSubmitting] = useState(false);
 
-  const handleChange = e => {
+  const handleChange = (e) => {
     const { name, value } = e.target;
     setValues({
       ...values,
-      [name]: value
+      [name]: value,
     });
   };
 
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    sendEmail(e)
+
+    // store submission values in an array to easily loop and check for an empty field
+    const submissionValues = Object.values(values);
+
+    let emptyFields = 0;
+
+    submissionValues.forEach((e) => {
+      // store how many emptyFields fields there are
+      // console.log("eeee", e);
+      if (e === "") {
+        emptyFields++;
+      } else {
+        // console.log("Missing Fields");
+        return;
+      }
+    });
+
+    // only send the email if you our emptyFields
+    if (emptyFields > 0) {
+      console.log("Please compelte the form");
+    } else {
+      sendEmail(e);
+    }
+
     setValues({
-        name: '',
-        email: '',
-        message: ''
-    })
+      name: "",
+      email: "",
+      message: "",
+    });
 
     setErrors(validate(values));
     setIsSubmitting(true);
   };
 
-  useEffect(
-    () => {
-      if (Object.keys(errors).length === 0 && isSubmitting) {
-        callback();
-      }
-    },
-    [errors, isSubmitting, callback]
-  );
+  useEffect(() => {
+    if (Object.keys(errors).length === 0 && isSubmitting) {
+      callback();
+    }
+  }, [errors, isSubmitting, callback]);
 
-      // EMAIL JS FUNCTIONALITY
-      function sendEmail(e) {
-        e.preventDefault();
-    
-        emailjs.sendForm('gmail_test', 'contact_form', e.target, 'user_iHKbBGEc2ruGFHITfjH3o')
-          .then((result) => {
-              console.log(result.text);
-          }, (error) => {
-              console.log(error.text);
-          });
-      }
+  // EMAIL JS FUNCTIONALITY
+  function sendEmail(e) {
+    e.preventDefault();
+    // console.log("Email Being Sent Your Way!!");
+    emailjs
+      .sendForm(
+        "gmail_test",
+        "contact_form",
+        e.target,
+        "user_iHKbBGEc2ruGFHITfjH3o"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  }
 
   return { handleChange, handleSubmit, values, errors };
 };
